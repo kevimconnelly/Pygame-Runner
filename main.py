7e3e792d669d9a20f9660a3ad21ea6998eb9466d
Kevin Connelly <kevinconnelly1337@gmail.com>
2023-09-18 10:52:15 -0700

    add snail movement

--- main.py
+++ main.py
@@ -5,11 +5,14 @@ pygame.init()
 screen = pygame.display.set_mode((800, 400))
 pygame.display.set_caption('Runner')
 clock = pygame.time.Clock()
-test_font = pygame.font.Font(None, 50)
+test_font = pygame.font.Font('font/Pixeltype.ttf', 50)
 
 sky_surface = pygame.image.load('graphics/Sky.png')
 ground_surface = pygame.image.load('graphics/ground.png')
-text_surface = test_font.render('My game', False, 'Green')
+text_surface = test_font.render('My game', False, 'Black')
+
+snail_surface = pygame.image.load('graphics/snail/snail1.png')
+snail_x_pos = 600
 
 while True:
     for event in pygame.event.get():
@@ -20,6 +23,8 @@ while True:
     screen.blit(sky_surface, (0, 0))
     screen.blit(ground_surface, (0, 300))
     screen.blit(text_surface, (300, 50))
+    snail_x_pos -= 4
+    screen.blit(snail_surface, (snail_x_pos, 250))
 
     pygame.display.update()
     clock.tick(60)
