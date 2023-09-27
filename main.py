8048ee0d511fbe3675917479538769a013100357
Kevin Connelly <kevinconnelly1337@gmail.com>
2023-09-26 20:48:13 -0700

    update snail image with rectangle

--- main.py
+++ main.py
@@ -12,11 +12,11 @@ ground_surface = pygame.image.load('graphics/ground.png').convert()
 text_surface = test_font.render('My game', False, 'Black')
 
 snail_surface = pygame.image.load('graphics/snail/snail1.png').convert_alpha()
-snail_x_pos = 600
+snail_rect = snail_surface.get_rect(midbottom=(600, 300))
 
 player_surf = pygame.image.load(
     'graphics/player/player_walk_1.png').convert_alpha()
-player_rect = player_surf.get_rect(midbottom=(80, 200))
+player_rect = player_surf.get_rect(midbottom=(80, 300))
 
 while True:
     for event in pygame.event.get():
@@ -27,11 +27,13 @@ while True:
     screen.blit(sky_surface, (0, 0))
     screen.blit(ground_surface, (0, 300))
     screen.blit(text_surface, (300, 50))
-    snail_x_pos -= 4
-    if snail_x_pos < -100:
-        snail_x_pos = 800
-    screen.blit(snail_surface, (snail_x_pos, 250))
+
+    screen.blit(snail_surface, snail_rect)
     screen.blit(player_surf, player_rect)
+
+    snail_rect.left -= 1
+    if snail_rect.right < 0:
+        snail_rect.left = 800
 
     pygame.display.update()
     clock.tick(60)
