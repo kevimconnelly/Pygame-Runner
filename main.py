88d473dd09f860605ecf57ac22473fab99891ec1
Kevin Connelly <kevinconnelly1337@gmail.com>
2023-09-26 21:30:59 -0700

    style scoring

--- main.py
+++ main.py
@@ -9,7 +9,9 @@ test_font = pygame.font.Font('font/Pixeltype.ttf', 50)
 
 sky_surface = pygame.image.load('graphics/Sky.png').convert()
 ground_surface = pygame.image.load('graphics/ground.png').convert()
-text_surface = test_font.render('My game', False, 'Black')
+
+score_surf = test_font.render('My game', False, (64, 64, 64))
+score_rect = score_surf.get_rect(center=(400, 50))
 
 snail_surface = pygame.image.load('graphics/snail/snail1.png').convert_alpha()
 snail_rect = snail_surface.get_rect(midbottom=(600, 300))
@@ -23,13 +25,15 @@ while True:
         if event.type == pygame.QUIT:
             pygame.quit()
             exit()
-        if event.type == pygame.MOUSEMOTION:
-            if player_rect.collidepoint(event.pos):
-                print('collision')
+        # if event.type == pygame.MOUSEMOTION:
+            # if player_rect.collidepoint(event.pos):
+            # print('collision')
 
     screen.blit(sky_surface, (0, 0))
     screen.blit(ground_surface, (0, 300))
-    screen.blit(text_surface, (300, 50))
+    pygame.draw.rect(screen, '#c0e8ec', score_rect)
+    pygame.draw.rect(screen, '#c0e8ec', score_rect, 10)
+    screen.blit(score_surf, score_rect)
 
     snail_rect.left -= 1
     if snail_rect.right < 0:
