01dc33d0de130af23f463c41ba9db4d1d3dc3e9a
Kevin Connelly <kevinconnelly1337@gmail.com>
2023-09-28 16:30:54 -0700

    add score

--- main.py
+++ main.py
@@ -1,6 +1,14 @@
 import pygame
 from sys import exit
 
+
+def display_score():
+    current_time = pygame.time.get_ticks()
+    score_surf = test_font.render(f'{current_time}', False, (64, 64, 64))
+    score_rect = score_surf.get_rect(center=(400, 50))
+    screen.blit(score_surf, score_rect)
+
+
 pygame.init()
 screen = pygame.display.set_mode((800, 400))
 pygame.display.set_caption('Runner')
@@ -11,8 +19,8 @@ game_active = True
 sky_surface = pygame.image.load('graphics/Sky.png').convert()
 ground_surface = pygame.image.load('graphics/ground.png').convert()
 
-score_surf = test_font.render('My game', False, (64, 64, 64))
-score_rect = score_surf.get_rect(center=(400, 50))
+#score_surf = test_font.render('My game', False, (64, 64, 64))
+#score_rect = score_surf.get_rect(center=(400, 50))
 
 snail_surface = pygame.image.load('graphics/snail/snail1.png').convert_alpha()
 snail_rect = snail_surface.get_rect(midbottom=(600, 300))
@@ -44,9 +52,10 @@ while True:
     if game_active:
         screen.blit(sky_surface, (0, 0))
         screen.blit(ground_surface, (0, 300))
-        pygame.draw.rect(screen, '#c0e8ec', score_rect)
-        pygame.draw.rect(screen, '#c0e8ec', score_rect, 10)
-        screen.blit(score_surf, score_rect)
+        # pygame.draw.rect(screen, '#c0e8ec', score_rect)
+        # pygame.draw.rect(screen, '#c0e8ec', score_rect, 10)
+        #screen.blit(score_surf, score_rect)
+        display_score()
 
         snail_rect.left -= 4
         if snail_rect.right < 0:
